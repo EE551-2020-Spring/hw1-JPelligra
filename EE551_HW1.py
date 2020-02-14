940bf32527f67e72f8dd8f336f1ba4f0a96ad642
JPelligra <47006365+JPelligra@users.noreply.github.com>
2020-02-13 20:15:03 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -5,7 +5,7 @@ Python Core object Types
 """
 
 import math
-
+import collections
 def numbers_and_strings():
     """
     This is to review numbers and strings and basic operations.
@@ -15,14 +15,16 @@ def numbers_and_strings():
 
     # Assign a string "Stevens" to a variable y
     y = "Stevens"
+
     # Repeat variable y 5 times
-    #test test test
+    print (y * 5)
     # What is the length of z?
-
+    z = ""
+    length = len(z)
     # Concatenate variable y with string " is good"
-
+    m = y + " is good"
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
-
+    n = m.replace('good', 'awesome')
     return x, y, z, length, m, n
 
 
@@ -33,14 +35,14 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
-
+    p = n.split()
     # Get all the items past the first of the third substring
-
+    r = p[2][1:]
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-
+    c = [[1, 4, 5],[6, 10, 11],[12, 17, 38]]
     # Collect the items in the last column of matrix A using list comprehension
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
@@ -48,8 +50,9 @@ def lists():
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
-
-    return p, r, c, d, o
+    q = [ord(c) for c in "Stevens"]
+    
+    return p, r, c, d, o, q
 
 
 def dictionaries():
@@ -60,25 +63,26 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-
+    f = {"fruit": "apple", "quantity": 4, "color": "green"}
     # Get the item in dictionary f that the key "fruit" maps to
-
+    a = f["fruit"]
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
-
+    f["quantity"] += 1
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-
+    p = {"name": {"first_name": "Grace", "last_name": "Hopper"}, 'jobs': ['scientist', 'engineer'], "age": 85}
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-
+    p['jobs'].append('programmer')
     # Get the third job Grace has that you recently added
-
+    k = p['jobs'][2]
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
+    r= "".join(sorted("amazing_grace",key=lambda x:x.lower()))
 
-    return a, f, p, k
+    return a, f, p, k, r
 
 numbers_and_strings()
 lists()
